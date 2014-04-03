61301f53b0246a44ce7fd35edd75e45c5f4c079c
rahul690973 <rahul.110392@gmail.com>
2014-04-03 23:44:06 +0530

    sencha app ver1

---
+++ final3/app/view/details/Main.js
@@ -0,0 +1,27 @@
+Ext.define('Locator.view.details.Main', {
+				extend: 'Ext.tab.Panel',
+				xtype: 'detailspanel',
+				config: {
+								cls : 'details-tabpanel default-bg',
+								tabBar : {
+												docked : 'bottom',
+												height:'0px'
+								},
+								items : [{
+												xtype : 'info'
+								}, {
+												xtype : 'gallery'
+								}, {
+												xtype : 'review'
+								}, {
+												xtype : 'titlebar',
+												docked : 'top',
+												title : 'Details',
+												items : [{
+																text : 'Places',
+																ui : 'back',
+																name : 'back_to_placelist'
+												}]
+								}]
+				}
+});
